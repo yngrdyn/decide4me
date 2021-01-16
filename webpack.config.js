564270330705b855155de49440d119dab310af31
Yngrid Coello <yngrdyn@gmail.com>
2021-01-17 00:16:43 +0100

    ingoring build code

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, 'src/app.js'),
   output: {
-    path: path.join(__dirname, '/public/'),
+    path: path.join(__dirname, '/public/build'),
     filename: 'bundle.js',
   },
   module: {

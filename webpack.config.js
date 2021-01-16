a768538d0971df0ec1109d561ea9839a67c8d6b2
Yngrid Coello <yngrdyn@gmail.com>
2021-01-17 00:12:39 +0100

    Setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/app.js'),
+  output: {
+    path: path.join(__dirname, '/public/'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  }
+}

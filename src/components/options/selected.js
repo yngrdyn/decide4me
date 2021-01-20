0143fbe1ead843f8189bbb3d28044190d19c06fb
Yngrid Coello <yngrdyn@gmail.com>
2021-01-20 22:02:54 +0100

    Styling modal

--- src/components/options/selected.js
+++ src/components/options/selected.js
@@ -6,10 +6,12 @@ const Selected = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.clearSelectedOption}
     contentLabel="Selected Option"
-    ariaHideApp={false}>
-    <h3>Selected Option</h3>
-    {props.selectedOption && <span>{props.selectedOption}</span>}
-    <button onClick={props.clearSelectedOption}>OK</button>
+    ariaHideApp={false}
+    closeTimeoutMS={200}
+    className="modal">
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <span className="modal__body">{props.selectedOption}</span>}
+    <button className="button" onClick={props.clearSelectedOption}>OK</button>
   </Modal>
 );
 
